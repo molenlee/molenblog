3463e2d001f9c76c5de5e6cb8b0ced5203c08e89
Molen <403274756@qq.com>
2015-11-19 23:53:14 +0800

    添加转盘

--- feature/route.js
+++ feature/route.js
@@ -3,6 +3,7 @@ var dao = require('./dao');
 var fs = require('fs');
 var path = require('path');
 
+
 var pageCache = {};
 var homePage = function*() {
 	this.response.set('content-type','text/html;charset=utf-8');
@@ -32,6 +33,39 @@ module.exports = function(app){
 	//某一类型
 	app.route('/api/:type').get(function* (){
 		
+	});
+
+	app.route('/data/:ptype/:type').get(function* (){
+		var STATICDATA = require('../data/' + this.params.ptype);
+		this.body = {
+			code : 200,
+			ret : STATICDATA()[this.params.type]
+		}
+	});
+
+	app.route('/relex/:type').get(function*(){
+		var type = this.params.type;
+		var result = {};
+		switch(type){
+			case 'rolling_wheel_result': 
+				var STATICDATA = require('../data/relax');
+				var rolling_wheels = STATICDATA()['rollingwheel'];
+				var random = Math.random();
+
+				for(var i in rolling_wheels){
+					var rolling_wheel = rolling_wheels[i];
+					if(random < rolling_wheel['random']){
+						result= {
+							code : 200,
+							ret: rolling_wheel
+						}
+						this.body = result;
+						return;
+					}
+				}
+				break;
+
+		}
 	});
 
 	app.route('/').get(homePage);
