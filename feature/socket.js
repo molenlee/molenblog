12ae04a8f90a37e11dcdbafc25246c49c097374b
Molen <403274756@qq.com>
2016-01-14 00:40:06 +0800

    fix

---
+++ feature/socket.js
@@ -0,0 +1,123 @@
+var socket = require('socket.io')
+
+module.exports = function(server){
+	var io = socket(server);
+
+
+	/**
+	 * code 
+ 	 */
+ 	nsp = '/code'
+	var code = io.of(nsp);
+
+	rooms = {
+		'0001': {
+			'id': '0001',
+			'name': '测试名称',
+			'title': '测试标题',
+			'desc' :'测试描述'
+		}
+	};
+
+
+	code.on('connection', function (socket) {
+		socket.on('disconnection', function(){
+			console.info('hehe  disconnection')
+		})
+		socket.on('disconnect', function(){
+			// 浏览器刷新也会触发
+			setTimeout(function(){
+				for(var key in rooms){
+					res = findClientsSocket(key, nsp)
+					if(res.length == 0){
+						delete rooms[key]
+					}
+					else{
+						socket.broadcast.to(key).emit('setRoomNumber', res.length);
+					}
+				}
+			}, 0)
+		})
+
+
+		socket.on('getRoomList', function(data, fn){
+			fn(rooms)
+		});
+
+		socket.on('creatRoom', function(data, fn){
+			var id = +new Date() + '' ;
+			data['id'] = id
+			rooms[id] = data;
+			fn(rooms);
+		});
+
+
+
+		socket.on('joinRoom', function(data, fn){
+			for(var key in rooms){
+				if (key == data)
+					continue;
+				socket.leave(key, function(){
+					checkSingleRomm(key);
+					socket.broadcast.to(data).emit('setRoomNumber', findClientsSocket(key, nsp).length);
+				});
+			}
+
+			socket.join(data, function(){
+				fn(rooms[data]);
+				socket.broadcast.to(data).emit('setRoomNumber', findClientsSocket(data, nsp).length);
+			});
+		});
+
+		socket.on('enterCode', function(data, roomID){
+			// io.to(roomID).emit('enterCode', data);
+			socket.broadcast.to(roomID).emit('enterCode', data);
+			// io.sockets.in(roomID).emit('enterCode', data);
+		});
+
+		
+
+
+		// socket.on('leaveGroup', function(data){	
+		// 	socket.leave(data, function(){
+		// 		console.info('leave')
+		// 	});
+		// })
+
+
+		socket.on('getRoomNumber', function (data, fn) {
+			res = findClientsSocket(data, nsp);
+			fn(res.length);
+		});
+
+		function checkSingleRomm(roomID){
+			var res = findClientsSocket(roomID, nsp);
+			if (res.length == 0){
+				delete rooms[roomID];
+			}
+		}
+
+		function findClientsSocket(roomId, namespace) {
+		    var res = [],
+		        ns = io.of(namespace || "/"); // the default namespace is "/"
+
+		    if (ns) {
+		        for (var id in ns.connected) {
+		            if (roomId) {
+		                var index = ns.connected[id].rooms.indexOf(roomId);
+		                if (index !== -1) {
+		                    res.push(ns.connected[id]);
+		                }
+		            } else {
+		                res.push(ns.connected[id]);
+		            }
+		        }
+		    }
+		    return res;
+		}
+
+
+
+
+	 });
+}
---
+++ static/js/relax/multiCode.js
@@ -0,0 +1,148 @@
+require.config({
+	paths: {
+		'jquery': '/lib/jQuery/jQuery',
+		'jqueryui': '/lib/jQueryUI/jquery-ui',
+		'io': '/lib/socket/socket.io',
+		'underscore': '/lib/underscore/underscore',
+		'text': '/lib/textjs/text',
+		'backbone': '/lib/backbone/backbone'
+	}
+});
+require(['require', 'jquery','io', 'underscore', 'backbone', 'jqueryui'], function(require, $, io, _, Backbone){
+	var ListView = Backbone.View.extend({
+		render: function(){
+			this.$el.html(_.template($('#roomTable').html())());
+			this.initData();
+			return this;
+		},
+		initData: function(){
+			var socket = window.socket;
+			socket.emit('getRoomList', {}, function(data){
+				data = data || {};
+				_html = '';
+				for(var key in data){
+					value = data[key];
+					_html += _.template($('#roomItem').html())(value);
+				}
+				$('#container .room-table tbody').html( _html );
+			});
+		},
+		events: {
+			'click #creat-room': 'creatRoom'
+		},
+		creatRoom: function(ev){
+			var v = this;
+			$($('#creat-room-template').html()).dialog({
+				modal: true,
+				buttons: [
+					{
+						text: 'OK',
+						click: function() {
+							data = $('#creat-form').serializeArray()
+							o = {}
+							for(var key in data){
+								value = data[key];
+								o[value.name] = value.value;
+							}
+							socket.emit('creatRoom', o, function(){
+								v.render();
+							});
+							$( this ).dialog( "close" );
+						}
+					}
+				]
+			});
+		}
+	});
+
+	var RoomModel = Backbone.Model
+
+	var RoomView = Backbone.View.extend({
+		render: function(){
+			var v = this,
+				m = v.model,
+				roomID = m.get('roomID'),
+				socket = window.socket;
+			socket.emit('joinRoom', roomID, function(data){
+				console.info(data)
+				v.$el.append(_.template( $('#single-room-template').html() )(data) );
+
+				socket.emit('getRoomNumber', roomID, function(data){
+					$('.person-number').html(data)
+				});
+			})
+			return v;
+		},
+		events: {
+			'keyup .enter-code': 'enterCode'
+		},
+		enterCode: function(ev){
+			var v = this,
+				m = v.model,
+				roomID = m.get('roomID'),
+				$target = $(ev.target),
+				socket = window.socket;
+			socket.emit('enterCode', $target.val(), roomID)
+		}
+
+	});
+
+
+
+
+
+
+	// socket.emit('joinGroup', 'react0')
+	// socket.emit('joinGroup', 'react1')
+	// socket.emit('leaveGroup', 'last')
+
+	// if(window.navigator.userAgent.indexOf('Safari') > -1){
+	// 	socket.emit('leaveGroup', 'react1')
+	// 	socket.emit('leaveGroup', 'last')
+	// }
+	
+	// socket.on('news', function (data) {
+	// 	socket.emit('getRoomNumber', 'last') 
+	// });
+
+	// //event attach
+	// $('#creat-room').bind('click', function(e){
+	// 	$('#creat-room-template').dialog()
+	// });
+
+	
+
+	var Router = Backbone.Router.extend({
+		routes: {
+			''	:	'listView',    
+			'list'	:	'listView',  
+			'room/:id': 'joinRoom'
+		},
+		listView: function(){
+			var listView = new ListView();
+			this.setView(listView);
+		},
+		'joinRoom': function(id){
+			var roomView = new RoomView({model: new RoomModel({roomID: id})});
+			this.setView(roomView);
+		},
+		setView: function(view){
+			$('#container').html(view.render().el)
+		}
+	});
+
+	var socket = window.socket = window.socket || io.connect('http://localhost:8080/code');
+	socket.on('enterCode', function (data) {
+		$('.enter-code').val(data)
+	});
+
+	socket.on('setRoomNumber', function(data){
+		$('.person-number').html(data)
+	});
+
+
+	window.router = new Router();
+	Backbone.history.start();
+	
+
+});
